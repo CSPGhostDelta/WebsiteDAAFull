88d3ff42859128645c8253a28cc7f5e75d74f6d4
CSP Ghost Delta <73121303+CSPGhostDelta@users.noreply.github.com>
2021-07-13 09:51:55 +0000

    Add files via upload

---
+++ app-kalkulator.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('kalkulator matematika/index.html')
+
+
+@app.route('/send', methods=['POST'])
+def send(sum=sum):
+    if request.method == 'POST':
+        matematika = request.form['matematika']
+        matematika2 = request.form['matematika2']
+        operasi = request.form['operasi']
+
+        if operasi == 'penjumlahan':
+            sum = float(matematika) + float(matematika2)
+            return render_template('kalkulator matematika/index.html', sum=sum)
+
+        elif operasi == 'pengurangan':
+            sum = float(matematika) - float(matematika2)
+            return render_template('kalkulator matematika/index.html', sum=sum)
+
+        elif operasi == 'perkalian':
+            sum = float(matematika) * float(matematika2)
+            return render_template('kalkulator matematika/index.html', sum=sum)
+
+        elif operasi == 'pembagian':
+            sum = float(matematika) / float(matematika2)
+            return render_template('kalkulator matematika/index.html', sum=sum)
+        else:
+            return render_template('kalkulator matematika/index.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
---
+++ app-persegipanjang.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+aplikasi = Flask(__name__)
+
+@aplikasi.route("/")
+def app():
+    return render_template("kalkulator/persegipanjang.html")
+
+@aplikasi.route("/send", methods=["POST"])
+def send(sum=sum):
+    if request.method == "POST":
+        persegipanjang = request.form["persegipanjang"]
+        persegipanjang2= request.form["persegipanjang2"]
+        sum = float(persegipanjang) * 2
+        sum2 = float(persegipanjang2) * 3
+        return render_template("kalkulator/persegipanjang.html", sum=sum, sum2=sum2)
+    else:
+        return render_template("kalkulator/persegipanjang.html")
+
+if __name__ == '__main__':
+    aplikasi.run(debug=True)
